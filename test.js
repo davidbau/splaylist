056abae609bcbd52230f88ea5df07dfd5a403b59
David Bau <david.bau@gmail.com>
2014-12-30 13:50:13 -0500

    More tests.

--- test.js
+++ test.js
@@ -1,52 +1,51 @@
 var SplayTree = require('./splay').SplayTree,
     assert = require('assert');
 
-/*
-var x = new SplayTree();
-for (var j = 0; j < 7; ++j) {
-  x.append('node' + j);
-}
-for (j = 0; j < 7; ++j) {
-  var loc = x.nth(j);
-  console.log(j + ": " + loc._V);
-  x.dump();
-}
-*/
-
-
 var n = 1e6;
-var start, end;
 
-start = +(new Date);
-var x = new SplayTree({
-  n: function() { return 1; }
-//  length: function(v) { return v.length }
+function time(text, fn) {
+  var start = +(new Date);
+  fn()
+  var end = +(new Date);
+  console.log(text, (end - start) / 1000 + 'sec');
+}
+
+var x;
+
+time(n + ' prepends and nths on plain tree', function() {
+  x = new SplayTree();
+  for (var j = 0; j < n; ++j) {
+    x.prepend('node' + j);
+    var k = Math.floor(Math.random() * x.size());
+    loc = x.nth(k);
+    assert(loc._V == 'node' + (j - k));
+  }
 });
-end = +(new Date);
-console.log('time: ' + (end - start) + 'ms for constructor');
 
-start = +(new Date);
-for (var j = 0; j < n; ++j) {
-  x.prepend('node' + j);
-  var k = Math.floor(Math.random() * x.size());
-  loc = x.nth(k);
-  assert(loc._V == 'node' + (j - k));
-}
-end = +(new Date);
-console.log('time: ' + (end - start) + 'ms for ' + n + ' appends and lookups');
+time('start-to-end traversal on a ' + n + ' plain tree', function() {
+  var loc = x.first();
+  while (loc !== null) {
+    loc = x.next(loc);
+  }
+});
 
-start = +(new Date);
-var total = 0;
-for (var loc = x.first(); loc !== null; loc = x.next(loc)) {
-  total += loc._V.length;
-}
-console.log('traverse total', total);
-end = +(new Date);
-console.log('time: ' + (end - start) + 'ms for ' + n + ' next()s');
+time(n + ' prepends and finds on total-length tree', function() {
+  x = new SplayTree({length: function(s) { return s.length; }});
+  var total = 0;
+  for (var j = 0; j < n; ++j) {
+    var s = 'node' + j;
+    total += s.length;
+    x.prepend(s);
+    var k = Math.floor(Math.random() * total);
+    loc = x.find('length', k);
+  }
+});
 
-var total = x.stat('length');
-console.log('cached total', total);
+time('start-to-end traversal on a ' + n + ' total-length tree', function() {
+  var loc = x.first();
+  while (loc !== null) {
+    loc = x.next(loc);
+  }
+});
 
-// x.dump();
-console.log('total length:', x.stat('length'));
 
