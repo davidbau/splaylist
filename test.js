74b0f4b6e0cb05ac25f4d51aae0a5e179f91fbb5
David Bau <david.bau@gmail.com>
2014-12-30 12:12:30 -0500

    Better test.

--- test.js
+++ test.js
@@ -1,41 +1,35 @@
 var SplayTree = require('./splay').SplayTree,
     assert = require('assert');
 
-/*
-var x = new SplayTree();
-for (var j = 0; j < 7; ++j) {
-  x.append('node' + j);
-}
-for (j = 0; j < 7; ++j) {
-  var loc = x.nth(j);
-  console.log(j + ": " + loc._V);
-  x.dump();
-}
-*/
-
-
 var n = 1e6;
 
-var start = +(new Date);
-
-/*
-var y = [];
-for (var j = 0; j < n; ++j) {
-  y.unshift(j);
-  var k = Math.floor(Math.random() * y.length);
-  assert(y[k] == j - k);
-}
-*/
-
-var x = new SplayTree(['length']);
-for (var j = 0; j < n; ++j) {
-  x.prepend('node' + j);
-  var k = Math.floor(Math.random() * x.size());
-  loc = x.nth(k);
-  assert(loc._V == 'node' + (j - k));
+function time(text, fn) {
+  var start = +(new Date);
+  fn()
+  var end = +(new Date);
+  console.log(text, (end - start) / 1000 + 'sec');
 }
 
-var end = +(new Date);
-console.log('time: ' + (end - start) + 'ms for ' + n + ' appends and lookups');
-console.log('total length:', x.stat('length'));
+time(n + ' prepends and nths on plain tree', function() {
+  var x = new SplayTree();
+  for (var j = 0; j < n; ++j) {
+    x.prepend('node' + j);
+    var k = Math.floor(Math.random() * x.size());
+    loc = x.nth(k);
+    assert(loc._V == 'node' + (j - k));
+  }
+});
+
+time(n + ' prepends and finds on total-length tree', function() {
+  var x = new SplayTree(['length']);
+  var total = 0;
+  for (var j = 0; j < n; ++j) {
+    var s = 'node' + j;
+    total += s.length;
+    x.prepend(s);
+    var k = Math.floor(Math.random() * total);
+    loc = x.find('length', k);
+  }
+});
+
 
