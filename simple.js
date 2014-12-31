4045b33dec20eb6ee59f5f256dcfff945101ad16
David Bau <david.bau@gmail.com>
2014-12-31 04:12:34 -0500

    Rename dump as toString.

--- simple.js
+++ simple.js
@@ -5,8 +5,8 @@ x = new SplayTree();
 for (var j = 0; j < 10; ++j) {
  x.append('node' + j);
 }
-x.dump();
+console.log(x.toString());
 for (j = 0; j < 10; ++j) {
   x.nth(j);
-  x.dump();
+  console.log(x.toString());
 }
